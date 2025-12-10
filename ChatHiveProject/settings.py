0e2e16f38aeb10630297324188ce83d24241e5b9
JJuan777 <juan72balderrama@gmail.com>
2025-12-10 10:20:59 -0600

    Deploy 1

--- ChatHiveProject/settings.py
+++ ChatHiveProject/settings.py
@@ -98,9 +98,11 @@ COOKIE_FLAGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5173", "http://localhost:5173",
+    "http://127.0.0.1:5173", "http://localhost:5173", "http://localhost:8081", "http://127.0.0.1:8081",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
